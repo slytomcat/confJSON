4c81c1d19fac4fb54d98fe0b919dc5c0a2beb7a4
Sly_tom_cat <slytomcat@mail.ru>
2017-12-25 18:22:35 +0300

    initial commit

---
+++ confJSON.go
@@ -0,0 +1,28 @@
+package сonfJSON
+
+import (
+  "encoding/json"
+  "os"
+  "log"
+)
+
+func Load(filePath string, cfg *map[string]interface{}) {
+  f, err := os.Open(filePath)
+  if err != nil {
+    log.Fatal("Configurations' file can't be read:", err)
+  }
+  defer f.Close()
+  json.NewDecoder(f).Decode(&cfg)
+}
+
+func Save(filePath string, cfg map[string]interface{}) {
+  f, err := os.Create(filePath)
+  if err != nil {
+    log.Fatal("Can't access to configuration file:", err)
+  }
+  defer f.Close()
+  buf, _ := json.Marshal(cfg)
+  f.Write(buf)
+
+}
+
