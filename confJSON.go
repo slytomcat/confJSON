451fbfcbfea6e00dc49988b0f70ef60739a7254c
Sly_tom_cat <slytomcat@mail.ru>
2017-12-26 09:06:17 +0300

    +gofmt

--- confJSON.go
+++ confJSON.go
@@ -1,28 +1,27 @@
 package сonfJSON
 
 import (
-  "encoding/json"
-  "os"
-  "log"
+	"encoding/json"
+	"log"
+	"os"
 )
 
 func Load(filePath string, cfg *map[string]interface{}) {
-  f, err := os.Open(filePath)
-  if err != nil {
-    log.Fatal("Configurations' file can't be read:", err)
-  }
-  defer f.Close()
-  json.NewDecoder(f).Decode(&cfg)
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal("Configurations' file can't be read:", err)
+	}
+	defer f.Close()
+	json.NewDecoder(f).Decode(&cfg)
 }
 
 func Save(filePath string, cfg map[string]interface{}) {
-  f, err := os.Create(filePath)
-  if err != nil {
-    log.Fatal("Can't access to configuration file:", err)
-  }
-  defer f.Close()
-  buf, _ := json.Marshal(cfg)
-  f.Write(buf)
+	f, err := os.Create(filePath)
+	if err != nil {
+		log.Fatal("Can't access to configuration file:", err)
+	}
+	defer f.Close()
+	buf, _ := json.Marshal(cfg)
+	f.Write(buf)
 
 }
-
