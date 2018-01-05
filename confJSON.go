f9a48ca9b0570659af8d5578afc345fd80d4b617
Sly_tom_cat <slytomcat@mail.ru>
2018-01-06 01:54:32 +0300

    docs

--- confJSON.go
+++ confJSON.go
@@ -5,23 +5,36 @@ import (
 	"log"
 	"os"
 )
+/*
+ * ConfJSON - is a simple tool to read/store configuration in the JSON format file.
+ *
+ * Functions:
+ *
+ *   Load(file string, cfg *map[string]interface{})
+ *
+ * Load reads JSON file in to cfg map. All JSON keys become keys of map and all values
+ * are stored as empty interface. So to use values yo have to convert them to requered format.
+ *
+ *   Save(file string, cfg map[string]interface{})
+ *
+ * Save stores configuration map in to JSON-formated file.
+ * */
 
-func Load(filePath string, cfg *map[string]interface{}) {
-	f, err := os.Open(filePath)
+func Load(file string, cfg *map[string]interface{}) {
+	f, err := os.Open(file)
 	if err != nil {
-		log.Fatal("Configurations' file can't be read:", err)
+		log.Fatal("Configuration file can't be read:", err)
 	}
 	defer f.Close()
 	json.NewDecoder(f).Decode(&cfg)
 }
 
-func Save(filePath string, cfg map[string]interface{}) {
-	f, err := os.Create(filePath)
+func Save(file string, cfg map[string]interface{}) {
+	f, err := os.Create(file)
 	if err != nil {
 		log.Fatal("Can't access to configuration file:", err)
 	}
 	defer f.Close()
 	buf, _ := json.Marshal(cfg)
 	f.Write(buf)
-
 }
